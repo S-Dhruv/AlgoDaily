06b2ea7a00131401942fef9e5acd6e38941c18a0
Dhruv S Shah <dhruvsshah171@gmail.com>
2025-03-02 21:12:08 +0530

    link to google auth updated

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(cors());
 //to accept data as json
 app.use(express.json()); 
 
-if(process.env.NODE_ENV === "deployment"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
